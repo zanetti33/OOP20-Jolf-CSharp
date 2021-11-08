d17c44698f2dc9acdfc3a22482ccfee56210907c
Alessandro Zanzi <alessandro.zanzi2@studio.unibo.it>
2021-11-08 18:04:47 +0100

    Add Ball and MovingObject

---
+++ Zanzi_Alessandro/Ball.cs
@@ -0,0 +1,123 @@
+﻿using System;
+
+using Zanzi_Alessandro.Point2D;
+
+using Zanzi_Alessandro.Vector2D;
+
+namespace Zanzi_Alessandro
+{
+	public class Ball : MovingObject
+	{
+
+		public const int DIAMETER = 15;
+		public const int RADIUS = DIAMETER / 2;
+
+		private const double DEFAULT_ACCELERATION = 10f;
+
+		private static Color ballColor = Color.WHITE;
+		private double acceleration;
+
+		/**
+		 * @param startingPos
+		 */
+		public Ball(Point2D startingPos)
+		{
+			super(startingPos);
+			this.map = null;
+		}
+
+		/**
+		 * sets the ball speed
+		 * @param newSpeed
+		 */
+		[MethodImpl(MethodImplOptions.synchronized)]
+		public void SetSpeed(Vector2D newSpeed)
+		{
+			this.speed = newSpeed;
+			this.acceleration = DEFAULT_ACCELERATION;
+		}
+
+		/**
+		 * sets the ball acceleration
+		 * @param newAcc
+		 */
+		[MethodImpl(MethodImplOptions.synchronized)]
+		public void SetAcceleration(double newAcc)
+		{
+			this.acceleration = newAcc;
+		}
+
+		/**
+		 * @return the ball acceleration
+		 */
+		[MethodImpl(MethodImplOptions.synchronized)]
+		public double GetAcceleration()
+		{
+			return this.acceleration;
+		}
+
+		/**
+		 * sets the ball color
+		 * @param newColor
+		 */
+		public static void SetColor(Color newColor)
+		{
+			ballColor = newColor;
+		}
+		
+		public void Draw()
+		{
+		}
+
+		[MethodImpl(MethodImplOptions.synchronized)]
+		protected void UpdateSpeed(double timeElapsed)
+		{
+			double deltaV = this.GetAcceleration() * timeElapsed;
+			if (this.GetSpeed().GetSquareModule() <= deltaV * deltaV)
+			{
+				this.speed = Vector2D.nullVector();
+				this.acceleration = 0f;
+			}
+			else
+			{
+				this.speed = new Vector2D(this.GetSpeed().GetAngle(), GetSpeed().GetModule() - deltaV);
+				this.acceleration = DEFAULT_ACCELERATION;
+			}
+		}
+
+		[MethodImpl(MethodImplOptions.synchronized)]
+		protected void ApplyConstraints()
+		{
+			this.map.GetObjects().stream()
+			.filter(obj=> !obj.equals(this))
+			.forEach(obj=>obj.ApplyConstraintTo(this));
+			if (this.GetPosition().GetY() + RADIUS >= this.map.GetSize().GetHeight())
+			{
+				this.SetPosition(new Point2D(this.GetPosition().GetX(), this.map.GetSize().GetHeight() - RADIUS));
+				this.speed = new Vector2D(this.GetSpeed().GetX(), -this.GetSpeed().GetY());
+			}
+			if (this.GetPosition().GetY() - RADIUS <= 0)
+			{
+				this.SetPosition(new Point2D(this.GetPosition().GetX(), RADIUS));
+				this.speed = new Vector2D(this.GetSpeed().GetX(), -this.GetSpeed().GetY());
+			}
+			if (this.GetPosition().GetX() + RADIUS >= this.map.GetSize().GetWidth())
+			{
+				this.SetPosition(new Point2D(this.map.GetSize().GetWidth() - RADIUS, this.GetPosition().GetY()));
+				this.speed = new Vector2D(-this.GetSpeed().GetX(), this.GetSpeed().GetY());
+			}
+			if (this.GetPosition().GetX() - RADIUS <= 0)
+			{
+				this.SetPosition(new Point2D(RADIUS, this.GetPosition().GetY()));
+				this.speed = new Vector2D(-this.GetSpeed().GetX(), this.GetSpeed().GetY());
+			}
+		}
+
+		//to implement if there are 2 or more balls in the same map simultaneously
+		public void ApplyConstraintTo(Ball ball)
+		{
+		}
+
+	}
+}
+
---
+++ Zanzi_Alessandro/Point2D.cs
@@ -0,0 +1,56 @@
+﻿using System;
+namespace ZanettiLorenzo;
+public class Point
+{
+	public int X { get; set; }
+	public int Y { get; set; }
+	public Point(int x, int y)
+	{
+		this.X = x;
+		this.Y = y;
+	}
+}
+public class Point2D
+{
+    public double X { get; private set; }
+	public double Y { get; private set; }
+	public Point2D(double x, double y)
+	{
+		this.X = x;
+		this.Y = y;
+	}
+	public Point2D(int x, int y)
+    {
+		this.X = x;
+		this.Y = y;
+    }
+	public static Point2D NullPoint()
+	{
+		return new Point2D(0, 0);
+	}
+	public int GetIntX()
+    {
+		return (int)this.X;
+    }
+	public int GetIntY()
+    {
+		return (int)this.Y;
+    }
+	public Point2D Translate(Vector2D vector)
+    {
+		return new Point2D(this.X + vector.X, this.Y + vector.Y);
+    }
+	public override String ToString()
+	{
+		return "Point2D [x=" + X + ", y=" + Y + "]";
+	}
+	public void SetLocation(Point2D newPoint)
+    {
+		X = newPoint.X;
+		Y = newPoint.Y;
+    }
+	public Point ToPoint()
+    {
+		return new Point((int)this.X, (int)this.Y);
+    }
+}
